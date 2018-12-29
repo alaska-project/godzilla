7a3d69c784fc915a31ba5bdf96b05795d0595b79
simone sabba (lenovo) <simone.sabba@live.com>
2018-12-29 01:24:38 +0100

    refactoring move command

--- src/common/Godzilla/Commands/MoveEntityCommandHandler.cs
+++ src/common/Godzilla/Commands/MoveEntityCommandHandler.cs
@@ -8,13 +8,13 @@ using System.Threading.Tasks;
 
 namespace Godzilla.Commands
 {
-    internal class MoveEntitiesCommandHandler<TContext> : IRequestHandler<MoveEntitiesCommand<TContext>, Unit>
+    internal class MoveEntityCommandHandler<TContext> : IRequestHandler<MoveEntityCommand<TContext>, Unit>
         where TContext : EntityContext
     {
         private readonly ITransactionService<TContext> _transactionService;
         private readonly IEntityCommandsHelper<TContext> _commandsHelper;
 
-        public MoveEntitiesCommandHandler(
+        public MoveEntityCommandHandler(
             ITransactionService<TContext> transactionService,
             IEntityCommandsHelper<TContext> commandsHelper)
         {
@@ -22,7 +22,7 @@ namespace Godzilla.Commands
             _commandsHelper = commandsHelper ?? throw new ArgumentNullException(nameof(commandsHelper));
         }
 
-        public Task<Unit> Handle(MoveEntitiesCommand<TContext> request, CancellationToken cancellationToken)
+        public Task<Unit> Handle(MoveEntityCommand<TContext> request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
