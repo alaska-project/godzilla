f1e63423f88eb3cf778c4df179244720a0f05adb
simone sabba (lenovo) <simone.sabba@live.com>
2019-02-08 14:18:35 +0100

    document repository

---
+++ src/common/Godzilla/DocumentRepository.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Godzilla
+{
+    public abstract class DocumentRepository<TContext, TEntity>
+        where TContext : EntityContext
+    {
+        #region Init
+
+        public DocumentRepository(TContext context)
+        {
+            Context = context;
+        }
+
+        protected TContext Context { get; }
+
+        #endregion
+
+        #region Aggregate
+
+        protected virtual async Task<DocumentAggregate<TEntity>> GetAggregate(Guid id)
+        {
+            var document = await GetDocument(id);
+            return document == null ?
+                null :
+                CreateAggregateInstance(document);
+        }
+
+        protected virtual async Task<DocumentAggregate<TEntity>> GetAggregate(string path)
+        {
+            var document = await GetDocument(path);
+            return document == null ?
+                null :
+                CreateAggregateInstance(document);
+        }
+
+        protected virtual async Task<DocumentAggregate<TEntity>> GetAggregate(Expression<Func<TEntity, bool>> filter)
+        {
+            var document = await GetDocument(filter);
+            return document == null ?
+                null :
+                CreateAggregateInstance(document);
+        }
+
+        protected virtual async Task<IEnumerable<DocumentAggregate<TEntity>>> GetAggregates()
+        {
+            var documents = await GetDocuments();
+            return documents
+                .Select(x => CreateAggregateInstance(x))
+                .ToList();
+        }
+
+        protected virtual async Task<IEnumerable<DocumentAggregate<TEntity>>> GetAggregates(IEnumerable<Guid> id)
+        {
+            var documents = await GetDocuments(id);
+            return documents
+                .Select(x => CreateAggregateInstance(x))
+                .ToList();
+        }
+
+        protected virtual async Task<IEnumerable<DocumentAggregate<TEntity>>> GetAggregates(Expression<Func<TEntity, bool>> filter)
+        {
+            var documents = await GetDocuments(filter);
+            return documents
+                .Select(x => CreateAggregateInstance(x))
+                .ToList();
+        }
+
+        protected virtual DocumentAggregate<TEntity> CreateAggregateInstance(Document<TEntity> document)
+        {
+            return (DocumentAggregate<TEntity>)
+                Activator.CreateInstance(typeof(DocumentAggregate<TEntity>), new object[] { document });
+        }
+
+        #endregion
+
+        #region Document
+
+        protected virtual async Task<Document<TEntity>> GetDocument(Guid id)
+        {
+            return await Context.Documents.GetDocument<TEntity>(id);
+        }
+
+        protected virtual async Task<Document<TEntity>> GetDocument(string path)
+        {
+            return await Context.Documents.GetDocument<TEntity>(path);
+        }
+
+        protected virtual async Task<Document<TEntity>> GetDocument(Expression<Func<TEntity, bool>> filter)
+        {
+            return await Context.Documents.GetDocument<TEntity>(filter);
+        }
+
+        protected virtual async Task<IEnumerable<Document<TEntity>>> GetDocuments()
+        {
+            return await Context.Documents.GetDocuments<TEntity>(x => true);
+        }
+
+        protected virtual async Task<IEnumerable<Document<TEntity>>> GetDocuments(IEnumerable<Guid> id)
+        {
+            return await Context.Documents.GetDocuments<TEntity>(id);
+        }
+
+        protected virtual async Task<IEnumerable<Document<TEntity>>> GetDocuments(Expression<Func<TEntity, bool>> filter)
+        {
+            return await Context.Documents.GetDocuments(filter);
+        }
+
+        #endregion
+
+        #region Entity
+
+        protected virtual async Task<TEntity> GetEntity(Guid id)
+        {
+            return await Context.Query.GetItem<TEntity>(id);
+        }
+
+        protected virtual async Task<TEntity> GetEntity(string path)
+        {
+            return await Context.Query.GetItem<TEntity>(path);
+        }
+
+        protected virtual async Task<TEntity> GetEntity(Expression<Func<TEntity, bool>> filter)
+        {
+            return await Context.Query.GetItem(filter);
+        }
+
+        protected virtual async Task<IEnumerable<TEntity>> GetEntities()
+        {
+            return await Context.Query.GetItems<TEntity>(x => true);
+        }
+
+        protected virtual async Task<IEnumerable<TEntity>> GetEntities(IEnumerable<Guid> id)
+        {
+            return await Context.Query.GetItems<TEntity>(id);
+        }
+
+        protected virtual async Task<IEnumerable<TEntity>> GetEntities(Expression<Func<TEntity, bool>> filter)
+        {
+            return await Context.Query.GetItems(filter);
+        }
+
+        #endregion
+    }
+}
