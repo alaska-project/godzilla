675f6790e329630ba0e65202308b70b5507ff57f
Simone Sabba <Simone.Sabba@alpitourworld.it>
2019-04-05 11:39:14 +0200

    context selector

--- src/ui/godzilla-ui/src/app/modules/database-explorer/components/database-tree-explorer/components/database-context-selector/database-context-selector.component.ts
+++ src/ui/godzilla-ui/src/app/modules/database-explorer/components/database-tree-explorer/components/database-context-selector/database-context-selector.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DatabaseContextService } from 'src/app/modules/database-explorer/services/database-context/database-context.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { UiEntityContextReference } from 'src/app/modules/database-explorer/clients/godzilla.clients';
 
 @Component({
@@ -8,14 +8,26 @@ import { UiEntityContextReference } from 'src/app/modules/database-explorer/clie
   templateUrl: './database-context-selector.component.html',
   styleUrls: ['./database-context-selector.component.scss']
 })
-export class DatabaseContextSelectorComponent implements OnInit {
+export class DatabaseContextSelectorComponent implements OnInit, OnDestroy {
 
   dartabaseContexts: Observable<UiEntityContextReference[]>;
 
-  constructor(private databaseContext: DatabaseContextService) { }
+  private currentContextSubscription: Subscription;
+  currentContext: UiEntityContextReference;
+
+  constructor(private databaseContext: DatabaseContextService) {
+  }
 
   ngOnInit() {
     this.dartabaseContexts = this.databaseContext.availableContexts();
+    this.currentContextSubscription = this.databaseContext.currentContext().subscribe(x => this.currentContext = x);
   }
 
+  ngOnDestroy(): void {
+    this.currentContextSubscription.unsubscribe();
+  }
+
+  selectContext(context: UiEntityContextReference) {
+    this.databaseContext.selectContext(context);
+  }
 }
